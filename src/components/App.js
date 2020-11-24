bcf2fbad6200327645bd11b25b603d5dfa8f7962
harimandir <harimandir@outlook.com>
2020-11-23 19:16:36 -0700

    Render users names on load

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
+import { useEffect } from "react";
 import { connect } from "react-redux";
 const { getUsersRequest } = require("../store/actions/users");
 
 function _App(props) {
+  const { getUsersRequest, users } = props;
+  useEffect(getUsersRequest, [getUsersRequest]);
+
   return (
     <div>
-      <button onClick={props.getUsersRequest}>Fetch</button>
+      {users.map((user) => (
+        <div key={user.id}>
+          {user.firstName} {user.lastName}
+        </div>
+      ))}
     </div>
   );
 }
 
 export const App = connect(
   ({ users }) => ({
-    users,
+    users: users.items,
   }),
   { getUsersRequest }
 )(_App);
