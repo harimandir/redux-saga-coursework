15d9fd8cfb021e0abdb5256a014552e266c83a2f
harimandir <harimandir@outlook.com>
2020-11-23 18:54:58 -0700

    Define axios base url for users API

--- src/api/users.js
+++ src/api/users.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "https://rem-rest-api.herokuapp.com/api";
+
 export const getUsers = () => {
   return axios.get("/users", {
     params: {
