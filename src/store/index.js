e64f0cec0850a2e0b02d290ff25f3fb77197c663
harimandir <harimandir@outlook.com>
2020-11-23 19:00:38 -0700

    Configure Redux DevTools extension

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { reducers } from "../store/reducers";
 import { rootSaga } from "./sagas";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
-export const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+export const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(rootSaga);
