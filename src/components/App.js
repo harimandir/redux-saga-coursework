25018180eab87fc018c45306dbfaf952942ccb22
harimandir <harimandir@outlook.com>
2020-11-23 22:16:29 -0700

    Dispatch create user request on new user form submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
-import { getUsersRequest } from "../store/actions/users";
+import { getUsersRequest, createUserRequest } from "../store/actions/users";
 import { UsersList } from "./UsersList";
 import { NewUserForm } from "./NewUserForm";
 
-function _App({ getUsersRequest, users }) {
+function _App({ getUsersRequest, users, createUserRequest }) {
   useEffect(getUsersRequest, [getUsersRequest]);
+
   return (
     <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
-      <NewUserForm onSubmit={handleSubmit} />
+      <NewUserForm onSubmit={createUserRequest} />
       <UsersList users={users} />
     </div>
   );
-}
-
-function handleSubmit({ firstName, lastName }) {
-  console.log({ firstName, lastName });
 }
 
 export const App = connect(
   ({ users }) => ({
     users: users.items,
   }),
-  { getUsersRequest }
+  { getUsersRequest, createUserRequest }
 )(_App);
