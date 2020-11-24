b77977de8d5abf9c7ffd98838fcb5cfd5a588f37
harimandir <harimandir@outlook.com>
2020-11-23 17:05:53 -0700

    Create store with root saga using redux middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,15 @@
-import { combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import { usersReducer } from "./users";
+import { rootSaga } from "../sagas";
 
-export const store = combineReducers({
-  users: usersReducer,
-});
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(
+  combineReducers({
+    users: usersReducer,
+  }),
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
