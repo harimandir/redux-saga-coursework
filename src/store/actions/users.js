8c17060771cf81b8ddf6b7f40b997f5554da10d9
harimandir <harimandir@outlook.com>
2020-11-24 22:28:17 -0700

    Add users error action creator

--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -3,6 +3,7 @@ export const Types = {
   GET_USERS_SUCCESS: "users/get_users_success",
   CREATE_USER_REQUEST: "users/create_user_request",
   DELETE_USER_REQUEST: "users/delete_user_request",
+  USERS_ERROR: "users/error",
 };
 
 export const getUsersRequest = () => ({ type: Types.GET_USERS_REQUEST });
@@ -26,5 +27,12 @@ export const deleteUserRequest = (id) => {
   return {
     type: Types.DELETE_USER_REQUEST,
     payload: { id },
+  };
+};
+
+export const usersError = (message) => {
+  return {
+    type: Types.USERS_ERROR,
+    payload: { message },
   };
 };
